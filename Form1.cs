12917fa0dcac5cc76e6b2e740a9c476c9fff0c21
Ahmed Sherif <Ahmed151198@gmail.com>
2017-12-20 23:09:40 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Events_Scheduler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string name = comboBox1.SelectedItem.ToString(); //getting the selected name
+            for (int i = 0; i < EventsData.Count; i++)
+            {
+                if (name = EventsData[i].EName) //matching Data
+                {
+                    EventsData.Remove(EventsData[i]); //Removing Data
+                    comboBox1.Items.Remove(@comboBox1.SelectedValue.ToString()); //Removing the item form Combobox
+                    break;
+                } 
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+{
+    class Event // Abraam's class
+    {
+        // Adding Event Class data members
+        public string EName; // contains  Event name
+        public string EPlace; // contains the place where the event will be
+        public string EStartDate; // contains the start date of Event
+        public string EStartTime; // contains the start time of Event
+        public string E_EndDate; // contains the end date of event
+        public string E_ReminderTime;  // ontains te remainder date 
+    }
+    
+}
+List<Event> EventsData = new List<Event>(); //The used list
+for (int i = 0; i < EventsData.Count; i++)
+            {
+                comboBox1.Items.Add(EventsData[i].EName); //Adding Name to combobox
+            }
+}
